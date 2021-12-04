0c60db64d63caa090d6ec4ca0344d0a716fdb249
arthur <arthurfjadecastro@gmail.com>
2021-12-03 22:50:48 -0300

    Update points of rank at 02/12/2021

--- src/views/Ranking.jsx
+++ src/views/Ranking.jsx
@@ -66,7 +66,7 @@ const Persons =  [
     name: "Marciano",
     src: "/img/MARCIANO.svg",
     position: null,
-    pts: 21
+    pts: 22
   },
   {
     name: "Beth",
@@ -78,7 +78,7 @@ const Persons =  [
     name: "Lana",
     src: "/img/LANA.svg",
     position: null,
-    pts: 20
+    pts: 21
   },
   {
     name: "Ramon",
@@ -90,7 +90,7 @@ const Persons =  [
     name: "João",
     src: "/img/JOAO.svg",
     position: null,
-    pts: 21
+    pts: 22
   },
   {
     name: "Carol",
@@ -115,19 +115,19 @@ const Persons =  [
     name: "Walmar",
     src: "/img/WALMAR.svg",
     position: null,
-    pts: 21
+    pts: 22
   },
   {
     name: "Rafaela",
     src: "/img/RAFAELA.svg",
     position: null,
-    pts: 15
+    pts: 16
   },
   {
     name: "Fernanda",
     src: "/img/FERNANDA.svg",
     position: null,
-    pts: 18
+    pts: 19
   },
   {
     name: "Kerson",
@@ -151,7 +151,7 @@ const Persons =  [
     name: "Eduardo",
     src: "/img/default-avatar.png",
     position: null,
-    pts: 21
+    pts: 22
   },
   {
     name: "Juliana",
@@ -212,7 +212,7 @@ const Persons =  [
     name: "Rômulo",
     src: "/img/default-avatar.png",
     position: null,
-    pts: 13
+    pts: 14
   },
 ]
 
@@ -277,7 +277,7 @@ Persons.sort(function (a, b) {
                Dias Restantes:    <b> {diffDays}</b>
             </Typography>
             <Typography variant="caption" className={classes.title2}>
-              Atualizado em:    <b> {"01/12/2021"}</b>
+              Atualizado em:    <b> {"02/12/2021"}</b>
             </Typography>
           </Toolbar>
         </AppBar>

d1d3b5cd9707bc264ab15ca2a82db07d1b3b421f
arthur <arthurfjadecastro@gmail.com>
2021-12-07 21:25:13 -0300

    Update points of ranking at 04/12/2021

--- src/views/Ranking.jsx
+++ src/views/Ranking.jsx
@@ -66,7 +66,7 @@ const Persons =  [
     name: "Marciano",
     src: "/img/MARCIANO.svg",
     position: null,
-    pts: 23
+    pts: 25
   },
   {
     name: "Beth",
@@ -78,7 +78,7 @@ const Persons =  [
     name: "Lana",
     src: "/img/LANA.svg",
     position: null,
-    pts: 22
+    pts: 24
   },
   {
     name: "Ramon",
@@ -90,7 +90,7 @@ const Persons =  [
     name: "João",
     src: "/img/JOAO.svg",
     position: null,
-    pts: 23
+    pts: 25
   },
   {
     name: "Carol",
@@ -102,7 +102,7 @@ const Persons =  [
     name: "Dayana",
     src: "/img/DAYANA.svg",
     position: null,
-    pts: 19
+    pts: 21
   },
   {
     name: "Arthur",
@@ -115,7 +115,7 @@ const Persons =  [
     name: "Walmar",
     src: "/img/WALMAR.svg",
     position: null,
-    pts: 23
+    pts: 25
   },
   {
     name: "Rafaela",
@@ -127,7 +127,7 @@ const Persons =  [
     name: "Fernanda",
     src: "/img/FERNANDA.svg",
     position: null,
-    pts: 20
+    pts: 22
   },
   {
     name: "Kerson",
@@ -139,7 +139,7 @@ const Persons =  [
     name: "Andréia",
     src: "/img/ANDREA.svg",
     position: null,
-    pts: 19
+    pts: 21
   },
   {
     name: "Luciano",
@@ -151,7 +151,7 @@ const Persons =  [
     name: "Eduardo",
     src: "/img/default-avatar.png",
     position: null,
-    pts: 23
+    pts: 25
   },
   {
     name: "Juliana",
@@ -277,7 +277,7 @@ Persons.sort(function (a, b) {
                Dias Restantes:    <b> {diffDays}</b>
             </Typography>
             <Typography variant="caption" className={classes.title2}>
-              Atualizado em:    <b> {"03/12/2021"}</b>
+              Atualizado em:    <b> {"04/12/2021"}</b>
             </Typography>
           </Toolbar>
         </AppBar>

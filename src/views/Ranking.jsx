83767804812eb6adfc10568d2195b7f0b8a45712
arthur <arthurfjadecastro@gmail.com>
2022-01-04 18:35:50 -0300

    Update points on ranking at 28/12/2021

--- src/views/Ranking.jsx
+++ src/views/Ranking.jsx
@@ -66,7 +66,7 @@ export default function Ranking({ open, handleClose }) {
       name: "Marciano",
       src: "/img/MARCIANO.svg",
       position: null,
-      pts: 53
+      pts: 54
     },
     {
       name: "Beth",
@@ -78,7 +78,7 @@ export default function Ranking({ open, handleClose }) {
       name: "Lana",
       src: "/img/LANA.svg",
       position: null,
-      pts: 48
+      pts: 49
     },
     {
       name: "Ramon",
@@ -90,7 +90,7 @@ export default function Ranking({ open, handleClose }) {
       name: "João",
       src: "/img/JOAO.svg",
       position: null,
-      pts: 54
+      pts: 55
     },
     {
       name: "Carol",
@@ -115,19 +115,19 @@ export default function Ranking({ open, handleClose }) {
       name: "Walmar",
       src: "/img/WALMAR.svg",
       position: null,
-      pts: 55
+      pts: 56
     },
     {
       name: "Rafaela",
       src: "/img/RAFAELA.svg",
       position: null,
-      pts: 40
+      pts: 41
     },
     {
       name: "Fernanda",
       src: "/img/FERNANDA.svg",
       position: null,
-      pts: 34
+      pts: 35
     },
     {
       name: "Kerson",
@@ -139,7 +139,7 @@ export default function Ranking({ open, handleClose }) {
       name: "Andréia",
       src: "/img/ANDREA.svg",
       position: null,
-      pts: 27
+      pts: 28
     },
     {
       name: "Luciano",
@@ -151,7 +151,7 @@ export default function Ranking({ open, handleClose }) {
       name: "Eduardo",
       src: "/img/default-avatar.png",
       position: null,
-      pts: 55
+      pts: 56
     },
     {
       name: "Juliana",
@@ -212,7 +212,7 @@ export default function Ranking({ open, handleClose }) {
       name: "Rômulo",
       src: "/img/default-avatar.png",
       position: null,
-      pts: 28
+      pts: 29
     },
   ]
 
@@ -277,7 +277,7 @@ export default function Ranking({ open, handleClose }) {
               Dias Restantes:    <b> {diffDays}</b>
             </Typography>
             <Typography variant="caption" className={classes.title2}>
-              Atualizado em:    <b> {"27/12/2021"}</b>
+              Atualizado em:    <b> {"28/12/2021"}</b>
             </Typography>
           </Toolbar>
         </AppBar>
